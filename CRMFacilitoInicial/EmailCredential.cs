d66dcadc177c3663584852d27855229d62a6a1aa
Julio Nava <jinc06@hotmail.com>
2017-04-23 03:29:13 -0500

    Commit inicial

---
+++ CRMFacilitoInicial/EmailCredential.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMFacilitoInicial
+{
+    public static class EmailCredential
+    {
+        public static string Email = "jinc06@hotmail.com";
+        public static string Password = "bethebest07";
+    }
+}
